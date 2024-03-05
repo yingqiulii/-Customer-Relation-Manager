f9af478f4c92dfc0e0fb777ec9a6379db1317df1
yingqiuli <yingqiuli@rrc.ca>
2024-03-04 20:44:28 -0600

    Your commit message here

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,52 @@
+Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/alphabetized'
+  get 'customers/missing_email'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+end
 # Rails.application.routes.draw do
-#   get 'customers/index'
-#   get 'customers/alphabetized'
-#   get 'customers/missing_email'
+#   resources :customers do
+#     collection do
+#       get 'alphabetized'
+#       get 'missing_email'
+#     end
+#   end
+
+#   root 'customers#index'
+# end
+# Rails.application.routes.draw do
 #   devise_for :admin_users, ActiveAdmin::Devise.config
 #   ActiveAdmin.routes(self)
 
+#   resources :customers do
+#     collection do
+#       get 'alphabetized'
+#       get 'missing_email'
+#     end
+#   end
+
+#   root 'customers#index'
 # end
+
+# Rails.application.routes.draw do
+#   devise_for :admin_users, ActiveAdmin::Devise.config
+#   ActiveAdmin.routes(self)
+
+#   namespace :api do
+#     resources :customers do
+#       collection do
+#         get 'alphabetized'
+#         get 'missing_email'
+#       end
+#     end
+#   end
+
+#   root 'customers#index'
+# end
+
+
 Rails.application.routes.draw do
   resources :customers do
     collection do
